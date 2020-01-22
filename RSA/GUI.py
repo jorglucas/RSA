60169f12a68118540b65bbdff678e0092bbaaab8
jorglucas <54417211+jorglucas@users.noreply.github.com>
2020-01-21 22:52:49 -0300

    Update GUI.py
    Primeiro código apenas com os principais métodos e widgets, nada funcional

--- RSA/GUI.py
+++ RSA/GUI.py
@@ -1,37 +1,57 @@
 # -*- coding: utf-8 -*-
 
-from tkinter import * #importa toda biblioteca do Tkinter
+#importa toda biblioteca do Tkinter
+from tkinter import *
 
-def feito():
-    l3['text'] = i1.get()
-    l3['fg'] = "green"
-    i1.delete(0, END)
 
-window_p = Tk() #Cria a janela principal
+#função pra capturar o texto digitado
+def feito():                       
+    l3['text'] = i1.get()           #método para pegar o texto
+    l3['fg'] = "green"              #cor do texto
+    i1.delete(0, END)               #deletar o texto da entrada de texto
+    
+    
+#Cria a janela principal
+window_p = Tk()             
 
-window_p.title("RSA Encrypt/Decrypt") #Titulo da janela
-window_p.geometry("800x600+200+200") #Dimensão da janela e distancia da borda superior e lateral esquerda
-window_p.wm_iconbitmap('icone.ico') #icone da janela
+# Titulo da janela
+window_p.title("RSA Encrypt/Decrypt")
 
-foto = PhotoImage(file = "image.png")
-ima = Label(window_p, image = foto)
+# Dimensão da janela e distancia da borda superior e lateral esquerda
+window_p.geometry("800x600+200+200")
 
-l1 = Label(window_p, text = "Digite seu texto abaixo",pady = 15)                        
+# Icone da janela
+window_p.wm_iconbitmap('icon.ico')
 
-i1 = Entry(window_p)
+# "Variável" que recebe o método PhotoImage e salva o arquivo dentro de si
+foto = PhotoImage(file = "image.png") 
 
+# Label que recebe a imagem da "variável" foto
+imag = Label(window_p, image = foto) 
+
+# Label de texto   
+l1 = Label(window_p, text = "Digite seu texto abaixo",pady = 15)             
+
+# Entrada de texto
+i1 = Entry(window_p) 
+
+# Botão Criptografar
 b1 = Button(window_p, text = "Criptografar", command = feito) 
 
-l3 = Label(window_p)
+# Label que mostra qual texto foi digitado em i1
+l3 = Label(window_p) 
 
-ima.pack()
+# CheckButton só de teste
+check1 = Checkbutton(window_p, text = "Teste", activeforeground = "green") 
+
+# Empacotamento dos widgets segundo a oredem que aparecem
+imag.pack() 
 l1.pack()
 i1.pack()
 b1.pack()
 l3.pack()
-
-check1 = Checkbutton(window_p, text = "Teste", activeforeground = "green")
 check1.pack()
 
 
+#Metodo para exibir a janela e todos widgets
 window_p.mainloop()
